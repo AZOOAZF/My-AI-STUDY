0136f84c9fcd89eea28bf74be3d5705cb6eccfe5
AZOOAZF <2071855126@qq.com>
2026-09-05 21:04:30 +0800

    Make Vercel API routing public and native

--- New project/api/index.js
+++ New project/api/index.js
@@ -1,4 +1,11 @@
-const serverless = require('serverless-http');
 const { app } = require('../server');
 
-module.exports = serverless(app);
+module.exports = async (req, res) => {
+  try {
+    await app(req, res);
+  } catch (error) {
+    console.error(error);
+    if (!res.headersSent) res.statusCode = 500;
+    res.end(JSON.stringify({ error: '服务器内部错误' }));
+  }
+};
